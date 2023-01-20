eacaf4cbf1b4e637095d831e369e0cd449c677f5
Dmitry <dimakameya@gmail.com>
2023-01-20 23:34:46 +0500

    третья цифра

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+//Функция показывает третью цифру числа abc
+int TrtyaCifraChisla(int number)
+{
+    int a = number % 10;
+    return a;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (number > 1000)
+    {
+       number = number / 10; 
+    }
+    int b = TrtyaCifraChisla(number);
+    Console.WriteLine(b);
+}
